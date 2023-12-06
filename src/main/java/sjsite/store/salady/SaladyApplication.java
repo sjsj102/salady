b31879e4975680c8a82c0cb63a76a14060340331
seuljijeon <seulji.1.0.2@gmail.com>
2023-12-06 12:04:19 +0900

    Initial commit

---
+++ src/main/java/sjsite/store/salady/SaladyApplication.java
@@ -0,0 +1,13 @@
+package sjsite.store.salady;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaladyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaladyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sjsite/store/salady/SaladyApplicationTests.java
@@ -0,0 +1,13 @@
+package sjsite.store.salady;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaladyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
